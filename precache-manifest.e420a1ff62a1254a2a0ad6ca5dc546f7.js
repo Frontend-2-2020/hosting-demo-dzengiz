abc7722d0ed450d804e6a6bbab74800377e85cb7
Dzengiz Tafa <dzengiz.tafa@gmail.com>
2020-03-30 20:09:13 +0200

    Updates

---
+++ precache-manifest.e420a1ff62a1254a2a0ad6ca5dc546f7.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a23ce3406079e8664c7c4bd96bb96c3",
+    "url": "/hosting-demo-dzengiz/index.html"
+  },
+  {
+    "revision": "cdc41bab2f9568fd7838",
+    "url": "/hosting-demo-dzengiz/static/css/2.24e4eb40.chunk.css"
+  },
+  {
+    "revision": "cdc41bab2f9568fd7838",
+    "url": "/hosting-demo-dzengiz/static/js/2.8450e240.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/hosting-demo-dzengiz/static/js/2.8450e240.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "855a8ce7235e3085508c",
+    "url": "/hosting-demo-dzengiz/static/js/main.269ed380.chunk.js"
+  },
+  {
+    "revision": "727959a2ab4d1bd56559",
+    "url": "/hosting-demo-dzengiz/static/js/runtime-main.79a0cc85.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/hosting-demo-dzengiz/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/hosting-demo-dzengiz/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/hosting-demo-dzengiz/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/hosting-demo-dzengiz/static/media/slick.f97e3bbf.svg"
+  }
+]);
